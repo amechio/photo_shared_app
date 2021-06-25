a62952651fb2debec7d99e8340378e2ea57cbd53
emi ota <ametio.145@gmail.com>
2021-06-25 10:59:52 +0000

    画像アップロード機能を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -64,6 +64,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:picture, :content)
+      params.require(:post).permit(:picture, :picture_cache, :content)
     end
 end
