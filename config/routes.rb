1092f69a8b8ac448ac6d77d2e3965be8e72e1ec0
emi ota <ametio.145@gmail.com>
2021-06-28 02:29:15 +0000

    プロフィール編集機能を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
 end
