a7617dfe123eb9a1b6a2100fd5124d4ad9f5763d
emi ota <ametio.145@gmail.com>
2021-06-29 13:07:40 +0000

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require_relative 'boot'
 
 require 'rails/all'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module PhotoSharedApp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
