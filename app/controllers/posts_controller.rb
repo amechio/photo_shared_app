fd869c21cd6148e9aa420ce9e09ddac3e3bb557b
emi ota <ametio.145@gmail.com>
2021-07-02 05:53:16 +0000

    インデントを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,11 +63,11 @@ class PostsController < ApplicationController
   end
 
   private
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def post_params
-      params.require(:post).permit(:picture, :picture_cache, :content)
-    end
+  def post_params
+    params.require(:post).permit(:picture, :picture_cache, :content)
+  end
 end
